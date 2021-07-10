097557397010577da5555d09e5187042fdee448a
Trywin <67023288+trywin0@users.noreply.github.com>
2021-07-11 00:07:13 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,13 +2,25 @@ const upgrades = [
     {name: "Human", bps: 1, cost: 50},
     {name: "Robot", bps: 3, cost: 100},
     {name: "Ape", bps: 10, cost: 500},
-    {name: "Banana farm", bps: 100, cost: 10000},
-    {name: "Obama", bps: 10000, cost: 1000000},
+    {name: "Banana farm", bps: 20, cost: 10000},
+    {name: "Banana bank", bps: 100, cost: 1000000},
+    {name: "Banana city", bps: 3000, cost: 10000000},
+    {name: "Banana country", bps: 50000, cost: 100000000},
 ]
-
+class User {
+    getAllData(){
+        return Object.keys(localStorage).reduce(function(obj, str) { 
+            obj[str] = localStorage.getItem(str); 
+            return obj
+        }, {});
+    }
+    setData(key, value){
+        return localStorage.setItem(key,value)
+    }
+}
 
 if (typeof(Storage) !== "undefined") {
-  
+  const user = new User()
     const nameStep = document.querySelector(".name")
     const gameDiv = document.querySelector(".game")
     const nameContinue = document.querySelector("button#name-continue")
@@ -38,18 +50,53 @@ if (typeof(Storage) !== "undefined") {
         }
     }
 
+    function update(){
+        const upgradesOwned = JSON.parse(user.getAllData()?.upgrades)
+        upgradesList.innerHTML=""
+        upgrades.forEach((upgrade, index)=>{
+            const canAfford = user.getAllData()?.bananas > upgrade.cost*((upgradesOwned[upgrade.name]||1)*0.05)
+            const upgradeDiv = document.createElement("div");
+            upgradeDiv.innerHTML = `
+
+            <div class="card-body">
+              <div class="text float-start">
+                <h5 class="card-title">${upgrade.name}</h5>
+                <h6 class="card-subtitle mb-2 text-warning">${upgrade.bps} BPS</h6>
+                <h6 class="owned card-subtitle text-warning">${upgradesOwned[upgrade.name]||0} owned</h6>
+                <h6 class="card-subtitle d-inline-block"><b>Cost:</b> <div class="cost ms-2 float-end">${Math.round(upgrade.cost*((upgradesOwned[upgrade.name]||1)*0.05))} Bananas</div></h3>
+
+            </div>
+              <button  onclick="purchase('${upgrade.name}')" class="purchasebtn btn ms-4 border rounded border-dark float-end bg-${canAfford?"success":"danger disabled"} text-light btn-m">Purchase</button>
+            </div>`
+            upgradeDiv.className="card"
+          const div = upgradesList.appendChild(upgradeDiv);
+          upgrades[index].div = div
+        })
+        monkeyBPS=0
+        Object.entries(upgradesOwned||{}).forEach(([uName, amount])=>{
+            const bps = upgrades.find(u=>u.name===uName)?.bps*amount
+            if(!bps) return;
+            monkeyBPS+=bps
+        })
+        gameDiv.classList.add("visible")
+        monkeyName.innerText = user.getAllData()?.name
+        bananaCount.innerText = parseInt(user.getAllData()?.bananas)
+        bpsCount.innerText = monkeyBPS
+    }
+
     function purchase(upgrade){
-        if(!localStorage.getItem("name")) return;
-        const balance = parseInt(localStorage.getItem("bananas"))
+        if(!user.getAllData()?.name) return;
+        const balance = parseInt(user.getAllData()?.bananas)
         const upgradeObject = upgrades.find(u=>u.name===upgrade)
         if(!upgradeObject) return console.log(upgrade);
-        const upgradesOwned = JSON.parse(localStorage.getItem("upgrades"))
+        const upgradesOwned = JSON.parse(user.getAllData()?.upgrades)
         if(balance < upgradeObject.cost*((upgradesOwned[upgrade]||1)*0.05)) return alert("You do not have enough bananas to purchase that upgrade")
+        const newBalance = balance-upgradeObject.cost*((upgradesOwned[upgrade]||1)*0.05)
         if(!upgradesOwned[upgrade]) upgradesOwned[upgrade] = 1
         else upgradesOwned[upgrade]++
-        const newBalance = balance-upgradeObject.cost*((upgradesOwned[upgrade]||1)*0.05)
-        localStorage.setItem("bananas", newBalance)
-        localStorage.setItem("upgrades", JSON.stringify(upgradesOwned))
+        user.setData("bananas", newBalance)
+        user.setData("upgrades", JSON.stringify(upgradesOwned))
+
         upgradeObject.div.querySelector(".owned").innerText=`${upgradesOwned[upgrade]} owned`
         upgradeObject.div.querySelector(".cost").innerText=`${Math.round(upgradeObject.cost*((upgradesOwned[upgrade]||1)*0.05))} Bananas`
 
@@ -59,44 +106,46 @@ if (typeof(Storage) !== "undefined") {
             if(!bps) return;
             monkeyBPS+=bps
         })
-        bpsCount.innerText = monkeyBPS
+        bpsCount.innerText = parseInt(monkeyBPS)
+        update()
 
     }
-    
+    let bananaClicked = false
+    const banana = document.querySelector(".banana")
+    function bananaSuprise(){
+        const delay = Math.floor(Math.random()*100000)
+        const x = Math.floor(Math.random()*window.innerWidth-200)+200
+        const y = Math.floor(Math.random()*window.innerHeight-200)+200
+        setTimeout(() => {
+            bananaClicked=false
+            banana.style.display = "block"
+            banana.className = "banana"
+            banana.style.top = y+"px";
+            banana.style.left = x+"px";
+            setTimeout(() => {
+                if(!bananaClicked){
+                banana.style.display = "none"
+            }
+            }, 3000);
+        }, delay);
+
+
+    }
+    banana.onclick = () => {
+        bananaClicked = true;
+        banana.style.display = "none"
+
+        const pay = Math.floor(Math.random()*10000)
+        user.setData("bananas", parseInt(user.getAllData()?.bananas)+pay)
+        bananaSuprise()
+    }
+    bananaSuprise()
+
     function startGame(){
-
-        const upgradesOwned = JSON.parse(localStorage.getItem("upgrades"))
-        upgrades.forEach((upgrade, index)=>{
-            const upgradeDiv = document.createElement("div");
-            upgradeDiv.innerHTML = `
-            <div class="card" style="width: fit-content;">
-            <div class="card-body">
-              <div class="text float-start">
-                <h5 class="card-title">${upgrade.name}</h5>
-                <h6 class="card-subtitle mb-2 text-success">${upgrade.bps} BPS</h6>
-                <h6 class="owned card-subtitle text-primary">${upgradesOwned[upgrade.name]||0} owned</h6>
-                <h6 class="card-subtitle d-inline-block"><b>Cost:</b> <div class="cost ms-2 float-end">${upgrade.cost*((upgradesOwned[upgrade]||1)*0.05)} Bananas</div></h3>
-
-            </div>
-              <button onclick="purchase('${upgrade.name}')" class="purchasebtn btn ms-4 border rounded border-dark float-end bg-success text-light btn-m">Purchase</button>
-            </div>
-          </div>`
-          const div = upgradesList.appendChild(upgradeDiv);
-          upgrades[index].div = div
-        })
-
-        Object.entries(upgradesOwned||{}).forEach(([uName, amount])=>{
-            const bps = upgrades.find(u=>u.name===uName)?.bps*amount
-            if(!bps) return;
-            monkeyBPS+=bps
-        })
-        gameDiv.classList.add("visible")
-        monkeyName.innerText = localStorage.getItem("name")
-        bananaCount.innerText = localStorage.getItem("bananas")
-        bpsCount.innerText = monkeyBPS
+        
         setInterval(()=>{
-            localStorage.setItem("bananas",parseInt(localStorage.getItem("bananas"))+monkeyBPS)
-            bananaCount.innerText = localStorage.getItem("bananas")
+            localStorage.setItem("bananas",parseInt(localStorage.getItem("bananas"))+parseInt(monkeyBPS))
+            update()
         }, 1000)
     }
 
